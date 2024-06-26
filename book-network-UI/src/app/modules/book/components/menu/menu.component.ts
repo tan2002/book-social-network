077c4d9289f097e3e843f32fcb19e320fbf6bf3d
tan2002 <tanmaydhanvijay0586@gmail.com>
2024-06-26 18:33:55 +0530

    Added the logic for the menu component

--- book-network-UI/src/app/modules/book/components/menu/menu.component.ts
+++ book-network-UI/src/app/modules/book/components/menu/menu.component.ts
@@ -8,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
+  ngOnInit(): void {
+    const linkColor = document.querySelectorAll('.nav-link');
+    linkColor.forEach(link => {
+      if (window.location.href.endsWith(link.getAttribute('href') || '')) {
+        link.classList.add('active');
+      }
+      link.addEventListener('click', () => {
+        linkColor.forEach(l => l.classList.remove('active'));
+        link.classList.add('active');
+      });
+    });
+  }
 
+  logout() {
+    localStorage.removeItem('token');
+    window.location.reload();
+  }
 }
