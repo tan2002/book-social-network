e50de1a769982a2a6c9d18fddeec9852e401694f
tan2002 <tanmaydhanvijay0586@gmail.com>
2024-06-26 19:06:33 +0530

    Added all the error code

--- book-network/src/main/java/org/tan/booknetwork/handler/BusinessErrorCode.java
+++ book-network/src/main/java/org/tan/booknetwork/handler/BusinessErrorCode.java
@@ -1,2 +1,30 @@
-package org.tan.booknetwork.handler;public enum BusinessErrorCode {
+package org.tan.booknetwork.handler;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+public enum BusinessErrorCode {
+    NO_CODE(0, "No code", NOT_IMPLEMENTED),
+    INCORRECT_CURRENT_PASSWORD(300,"Current password in incorrect",BAD_REQUEST),
+    NEW_PASSWORD_DOES_NOT_MATCHED(301,"The new password does not match",BAD_REQUEST),
+    ACCOUNT_LOCKED(302,"User account is locked.",FORBIDDEN),
+    ACCOUNT_DISABLED(303,"User acount is disabled",FORBIDDEN),
+    BAD_CREDENTIALS(304,"Invalid credentials.",FORBIDDEN);
+
+    @Getter
+    private final int code;
+    @Getter
+    private final  String description;
+    @Getter
+    private final HttpStatus httpStatus;
+
+
+    BusinessErrorCode(int code, String description, HttpStatus httpStatus) {
+        this.code = code;
+        this.description = description;
+        this.httpStatus = httpStatus;
+    }
 }
