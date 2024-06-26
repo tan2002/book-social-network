7316f907ce4d09afeb42f1e2053b224e3758711c
tan2002 <tanmaydhanvijay0586@gmail.com>
2024-06-26 19:35:26 +0530

    It contains the specification for book

--- book-network/src/main/java/org/tan/booknetwork/book/BookSpecification.java
+++ book-network/src/main/java/org/tan/booknetwork/book/BookSpecification.java
@@ -1,2 +1,11 @@
-package org.tan.booknetwork.book;public class BookSpecification {
+package org.tan.booknetwork.book;
+
+import org.springframework.data.jpa.domain.Specification;
+
+public class BookSpecification {
+    //In order to create a specification for the repository we have to
+    // define a method as a static for better execution.
+    public static Specification<Book> withOwnerId(Integer ownerId){
+        return ((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("owner").get("id"),ownerId));
+    }
 }
