c3c1ed0035b6446ad9d384e5883da190767e0463
tan2002 <tanmaydhanvijay0586@gmail.com>
2024-06-26 19:29:16 +0530

    Created the common class so that another class inherit it.

--- book-network/src/main/java/org/tan/booknetwork/common/BaseEntity.java
+++ book-network/src/main/java/org/tan/booknetwork/common/BaseEntity.java
@@ -1,2 +1,43 @@
-package org.tan.booknetwork.common;public class BaseEntity {
+package org.tan.booknetwork.common;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@SuperBuilder
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @CreatedDate
+    @Column(insertable = false,updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+
+    @CreatedBy
+    @Column(nullable = false,updatable = false)
+    private Integer createdBy;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    private Integer modifiedBy;
 }
