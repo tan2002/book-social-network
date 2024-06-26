1ba0f017e9d67b580aa1b1ab55c10a1edd14968c
tan2002 <tanmaydhanvijay0586@gmail.com>
2024-06-26 19:03:37 +0530

    Added the exception response class

--- book-network/src/main/java/org/tan/booknetwork/handler/ExceptionResponse.java
+++ book-network/src/main/java/org/tan/booknetwork/handler/ExceptionResponse.java
@@ -1,2 +1,21 @@
-package org.tan.booknetwork.handler;public class ExceptionResponse {
+package org.tan.booknetwork.handler;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ExceptionResponse {
+    private Integer businessErrorCode;
+    private String businessErrorDescription;
+    private String error;
+    private Set<String> validationError;
+    private Map<String , String> errors;
 }
