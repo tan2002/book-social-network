a6408fee6ce9cd68885b060aeb0bbb145efc6933
tan2002 <tanmaydhanvijay0586@gmail.com>
2024-06-26 22:03:14 +0530

    Created and added the view for the book-list component

---
+++ book-network-UI/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -0,0 +1,104 @@
+import {Component, OnInit} from '@angular/core';
+import {PageResponseBookResponse} from "../../../../services/models/page-response-book-response";
+import {BookService} from "../../../../services/services/book.service";
+import {Router} from "@angular/router";
+import {BookResponse} from "../../../../services/models/book-response";
+import {BookCardComponent} from "../../components/book-card/book-card.component";
+import {NgForOf, NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-book-list',
+  standalone: true,
+  imports: [
+    BookCardComponent,
+    NgForOf,
+    NgIf
+  ],
+  templateUrl: './book-list.component.html',
+  styleUrl: './book-list.component.scss'
+})
+export class BookListComponent implements OnInit{
+  bookResponse: PageResponseBookResponse = {};
+  page = 0;
+  size = 5;
+  pages: any = [];
+  message = '';
+  level: 'success' |'error' = 'success';
+
+  constructor(
+    private bookService: BookService,
+    private router: Router
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.findAllBooks();
+  }
+
+  private findAllBooks() {
+    this.bookService.findAllBooksByOwner(
+      {
+      size: this.size,
+      page: this.page,
+
+    }).subscribe({
+        next: (books:PageResponseBookResponse) => {
+          this.bookResponse = books;
+          this.pages = Array(this.bookResponse.totalPages)
+            .fill(0)
+            .map((x, i) => i);
+        }
+      });
+  }
+
+  gotToPage(page: number) {
+    this.page = page;
+    this.findAllBooks();
+  }
+
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllBooks();
+  }
+
+  goToPreviousPage() {
+    this.page--;
+    this.findAllBooks();
+  }
+
+  goToLastPage() {
+    this.page = this.bookResponse.totalPages as number - 1;
+    this.findAllBooks();
+  }
+
+  goToNextPage() {
+    this.page++;
+    this.findAllBooks();
+  }
+
+  get isLastPage() {
+    return this.page === this.bookResponse.totalPages as number - 1;
+  }
+
+  borrowBook(book: BookResponse) {
+    this.message = '';
+    this.level = 'success';
+    this.bookService.borrowBook({
+      'book-id': book.id as number
+    }).subscribe({
+      next: () => {
+        this.level = 'success';
+        this.message = 'Book successfully added to your list';
+      },
+      error: (err) => {
+        console.log(err);
+        this.level = 'error';
+        this.message = err.error.error;
+      }
+    });
+  }
+
+  displayBookDetails(book: BookResponse) {
+    this.router.navigate(['books', 'details', book.id]);
+  }
+}
