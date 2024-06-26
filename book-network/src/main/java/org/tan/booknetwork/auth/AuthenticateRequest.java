77763abf9011a154db32e5cc2df66dca4b4633c8
tan2002 <tanmaydhanvijay0586@gmail.com>
2024-06-26 20:06:03 +0530

    Created the authentication request class for authentication

--- book-network/src/main/java/org/tan/booknetwork/auth/AuthenticateRequest.java
+++ book-network/src/main/java/org/tan/booknetwork/auth/AuthenticateRequest.java
@@ -1,2 +1,24 @@
-package org.tan.booknetwork.auth;public class AuthenticateRequest {
+package org.tan.booknetwork.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class AuthenticateRequest {
+    @Email(message = "Email is not valid.")
+    @NotEmpty(message = "Email is mandatory.")
+    @NotBlank(message = "Email is mandatory.")
+    private String email;
+
+    @NotEmpty(message = "Password is mandatory.")
+    @NotBlank(message = "Password is mandatory.")
+    @Size(message = "Password must contain special character and it should be 10 character long")
+    private String password;
 }
