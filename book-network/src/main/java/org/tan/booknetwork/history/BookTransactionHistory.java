5dc6e2f4b5c7e96b04abbd4ab318eae3c12f8eac
tan2002 <tanmaydhanvijay0586@gmail.com>
2024-06-26 19:01:52 +0530

    Created the BookTransactionHistory entity

--- book-network/src/main/java/org/tan/booknetwork/history/BookTransactionHistory.java
+++ book-network/src/main/java/org/tan/booknetwork/history/BookTransactionHistory.java
@@ -1,2 +1,34 @@
-package org.tan.booknetwork.history;public class BookTransactionHistory {
+package org.tan.booknetwork.history;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.tan.booknetwork.book.Book;
+import org.tan.booknetwork.common.BaseEntity;
+import org.tan.booknetwork.user.User;
+
+import java.util.List;
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookTransactionHistory extends BaseEntity {
+
+    //User relationship
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    //Book Relationship
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+    private boolean returned;
+    private boolean returnApproved;
+
 }
