43dff7b8ae80bfde1cf79dbe7fefc30d9713de0d
tan2002 <tanmaydhanvijay0586@gmail.com>
2024-06-26 19:22:21 +0530

    Added the configuration for openAPI

--- book-network/src/main/java/org/tan/booknetwork/config/OpenApiConfig.java
+++ book-network/src/main/java/org/tan/booknetwork/config/OpenApiConfig.java
@@ -1,2 +1,48 @@
-package org.tan.booknetwork.config;public class OpenApiConfig {
+package org.tan.booknetwork.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Tanmay",
+                        email = "tanmaydhanvijay0586@gmail.com"
+                ),
+                description = "OpenApi documentation for Spring security",
+                title = "OpenApi specification - Tanmay",
+                version = "1.0.0",
+                license = @License(
+                        name = "License Name"
+                )
+        ),
+        servers = {
+                @Server(
+                        description = "local Environment",
+                        url = "http://localhost:8080/api/v1"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuthentication"
+                )
+        }
+)
+@SecurityScheme(
+        //The name should be same as the same provided in the SecurityRequirement.
+        name = "bearerAuthentication",
+        description = "JWT Authentication description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
 }
