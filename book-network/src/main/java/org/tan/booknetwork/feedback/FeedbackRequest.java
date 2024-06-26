397da84d66ed6ec224ad7bb836829de48db25e94
tan2002 <tanmaydhanvijay0586@gmail.com>
2024-06-26 19:09:23 +0530

    Added the feedback Request

--- book-network/src/main/java/org/tan/booknetwork/feedback/FeedbackRequest.java
+++ book-network/src/main/java/org/tan/booknetwork/feedback/FeedbackRequest.java
@@ -1,2 +1,16 @@
-package org.tan.booknetwork.feedback;public class FeedbackRequest {
+package org.tan.booknetwork.feedback;
+
+import jakarta.validation.constraints.*;
+
+public record FeedbackRequest(
+        @Positive(message = "200")
+        @Min(value = 0,message = "201")
+        @Max(value = 5,message = "202") Double note,
+        @NotNull(message = "203")
+        @NotEmpty(message = "203")
+        @NotBlank(message = "203")
+        String comment,
+        @NotNull(message = "204")
+        Integer bookId
+) {
 }
