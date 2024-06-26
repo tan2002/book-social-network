baee504059d94a90643658827c56d393ed4d2bcf
tan2002 <tanmaydhanvijay0586@gmail.com>
2024-06-26 19:19:56 +0530

    Created an Email Template

--- book-network/src/main/java/org/tan/booknetwork/email/EmailTemplateName.java
+++ book-network/src/main/java/org/tan/booknetwork/email/EmailTemplateName.java
@@ -1,2 +1,13 @@
-package org.tan.booknetwork.email;public class EmailTemplateName {
+package org.tan.booknetwork.email;
+
+import lombok.Getter;
+
+@Getter
+public enum EmailTemplateName {
+    ACTIVATE_ACCOUNT("activate_account");
+    private final String name;
+
+    EmailTemplateName(String name){
+        this.name = name;
+    }
 }
