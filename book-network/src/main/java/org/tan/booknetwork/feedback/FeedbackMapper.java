f2a79a7f54431b9de3adf458993e192824aa6610
tan2002 <tanmaydhanvijay0586@gmail.com>
2024-06-26 19:10:58 +0530

    Creted the mapper for Feedback entity.

--- book-network/src/main/java/org/tan/booknetwork/feedback/FeedbackMapper.java
+++ book-network/src/main/java/org/tan/booknetwork/feedback/FeedbackMapper.java
@@ -1,2 +1,29 @@
-package org.tan.booknetwork.feedback;public class FeedbackMapper {
+package org.tan.booknetwork.feedback;
+
+import org.springframework.stereotype.Service;
+import org.tan.booknetwork.book.Book;
+
+import java.util.Objects;
+
+@Service
+public class FeedbackMapper {
+    public Feedback toFeedback(FeedbackRequest request) {
+        return Feedback.builder()
+                .note(request.note())
+                .comment(request.comment())
+                .book(Book.builder()
+                        .id(request.bookId())
+                        .archived(false)
+                        .shareable(false)
+                        .build())
+                .build();
+    }
+
+    public FeedbackResponse toFeedbackResponse(Feedback f, Integer id) {
+        return FeedbackResponse.builder()
+                .note(f.getNote())
+                .comment(f.getComment())
+                .ownFeedback(Objects.equals(f.getCreatedBy(),id))
+                .build();
+    }
 }
