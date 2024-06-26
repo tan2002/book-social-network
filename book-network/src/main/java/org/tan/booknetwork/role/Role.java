377fde89da689b93d4ee09bab9360494284458d9
tan2002 <tanmaydhanvijay0586@gmail.com>
2024-06-26 18:59:19 +0530

    Created the role entity.

--- book-network/src/main/java/org/tan/booknetwork/role/Role.java
+++ book-network/src/main/java/org/tan/booknetwork/role/Role.java
@@ -1,2 +1,38 @@
-package org.tan.booknetwork.role;public class Role {
+package org.tan.booknetwork.role;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.tan.booknetwork.user.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @CreatedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
 }
