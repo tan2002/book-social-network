ab1c550802c32d90ed2fd8c4c5b6e5b3b1af1a4a
tan2002 <tanmaydhanvijay0586@gmail.com>
2024-06-26 19:08:31 +0530

    Added the Feedback Response

--- book-network/src/main/java/org/tan/booknetwork/feedback/FeedbackResponse.java
+++ book-network/src/main/java/org/tan/booknetwork/feedback/FeedbackResponse.java
@@ -1,2 +1,16 @@
-package org.tan.booknetwork.feedback;public class FeedbackResponse {
+package org.tan.booknetwork.feedback;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FeedbackResponse {
+    private Double note;
+    private String comment;
+    private boolean ownFeedback;
 }
