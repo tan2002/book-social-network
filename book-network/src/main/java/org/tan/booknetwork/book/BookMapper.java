9f3de3fc9bdd62c941b216e7e15eb6b2b64f51e7
tan2002 <tanmaydhanvijay0586@gmail.com>
2024-06-26 19:54:34 +0530

    Added the mapper layer for Book entity

--- book-network/src/main/java/org/tan/booknetwork/book/BookMapper.java
+++ book-network/src/main/java/org/tan/booknetwork/book/BookMapper.java
@@ -1,2 +1,47 @@
-package org.tan.booknetwork.book;public class BookMapper {
+package org.tan.booknetwork.book;
+
+import org.springframework.stereotype.Service;
+import org.tan.booknetwork.book.file.FileUtils;
+import org.tan.booknetwork.history.BookTransactionHistory;
+
+@Service
+public class BookMapper {
+
+    public Book toBook(BookRequest request) {
+        return Book.builder()
+                .id(request.id())
+                .title(request.title())
+                .authorName(request.authorName())
+                .synopsis(request.synopsis())
+                .archived(false)
+                .shareable(request.shareable())
+                .build();
+    }
+
+    public BookResponse toBookResponse(Book book) {
+        return BookResponse.builder()
+                .id(book.getId())
+                .title(book.getTitle())
+                .authorName(book.getAuthorName())
+                .isbn(book.getIsbn())
+                .synopsis(book.getSynopsis())
+                .rate(book.getRate())
+                .archived(book.isArchived())
+                .shareable(book.isShareable())
+                .owner(book.getOwner().getFullName())
+                .cover(FileUtils.readFileFromLocation(book.getBookCover()))
+                .build();
+    }
+
+    public BorrowedBookResponse toBorrowedBookResponse(BookTransactionHistory bookTransactionHistory) {
+        return BorrowedBookResponse.builder()
+                .id(bookTransactionHistory.getBook().getId())
+                .title(bookTransactionHistory.getBook().getTitle())
+                .authorName(bookTransactionHistory.getBook().getAuthorName())
+                .isbn(bookTransactionHistory.getBook().getIsbn())
+                .rate(bookTransactionHistory.getBook().getRate())
+                .returned(bookTransactionHistory.isReturned())
+                .returnedApproved(bookTransactionHistory.isReturnApproved())
+                .build();
+    }
 }
