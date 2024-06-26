d890694a6f9b66c9ab25eaf357b136da7aaa34de
tan2002 <tanmaydhanvijay0586@gmail.com>
2024-06-26 19:37:22 +0530

    Added the response for book entity.

--- book-network/src/main/java/org/tan/booknetwork/book/BookResponse.java
+++ book-network/src/main/java/org/tan/booknetwork/book/BookResponse.java
@@ -1,2 +1,21 @@
-package org.tan.booknetwork.book;public class BookResponse {
+package org.tan.booknetwork.book;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookResponse {
+    private Integer id;
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String owner;
+    private byte[] cover;
+    private double rate;
+    private boolean archived;
+    private boolean shareable;
 }
