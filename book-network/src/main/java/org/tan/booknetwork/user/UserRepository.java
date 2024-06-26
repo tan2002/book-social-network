18413abd793a93a0600751a5148aed4d24438f88
tan2002 <tanmaydhanvijay0586@gmail.com>
2024-06-26 18:45:51 +0530

    Created the findByEmail method of type optional

--- book-network/src/main/java/org/tan/booknetwork/user/UserRepository.java
+++ book-network/src/main/java/org/tan/booknetwork/user/UserRepository.java
@@ -1,2 +1,10 @@
-package org.tan.booknetwork.user;public class UserRepository {
+package org.tan.booknetwork.user;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
 }
