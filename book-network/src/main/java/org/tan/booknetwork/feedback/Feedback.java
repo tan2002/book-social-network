dd0a413b45f780c5c034c269f7feaa843f65d251
tan2002 <tanmaydhanvijay0586@gmail.com>
2024-06-26 19:13:00 +0530

    Created the Feedback Entity

--- book-network/src/main/java/org/tan/booknetwork/feedback/Feedback.java
+++ book-network/src/main/java/org/tan/booknetwork/feedback/Feedback.java
@@ -1,2 +1,25 @@
-package org.tan.booknetwork.feedback;public class Feedback {
+package org.tan.booknetwork.feedback;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.tan.booknetwork.book.Book;
+import org.tan.booknetwork.common.BaseEntity;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Feedback extends BaseEntity {
+    private Double note;
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
 }
