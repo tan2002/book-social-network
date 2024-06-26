506e952e4f602f817496b04ffcef70bec134dbc2
tan2002 <tanmaydhanvijay0586@gmail.com>
2024-06-26 19:57:40 +0530

    Created the Book entity.

--- book-network/src/main/java/org/tan/booknetwork/book/Book.java
+++ book-network/src/main/java/org/tan/booknetwork/book/Book.java
@@ -1,2 +1,54 @@
-package org.tan.booknetwork.book;public class Book {
+package org.tan.booknetwork.book;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.tan.booknetwork.common.BaseEntity;
+import org.tan.booknetwork.feedback.Feedback;
+import org.tan.booknetwork.history.BookTransactionHistory;
+import org.tan.booknetwork.user.User;
+
+import java.util.List;
+
+@Entity
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Book extends BaseEntity {
+
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private boolean archived;
+    private boolean shareable;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    @OneToMany(mappedBy = "book")
+    private List<Feedback> feedbacks;
+
+    @OneToMany(mappedBy = "book")
+    private List<BookTransactionHistory> histories;
+
+    @Transient
+    public double getRate(){
+        if(feedbacks == null || feedbacks.isEmpty())
+            return 0.0;
+        var rate = this.feedbacks.stream()
+                .mapToDouble(Feedback::getNote)
+                .average()
+                .orElse(0.0);
+        double roundRate = Math.round(rate + 10.0)/10.0;
+        return roundRate;
+    }
+
 }
