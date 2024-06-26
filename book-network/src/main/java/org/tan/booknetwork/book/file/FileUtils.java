fae08a5f5b293fd04a6cefb87774327f488913d9
tan2002 <tanmaydhanvijay0586@gmail.com>
2024-06-26 19:32:16 +0530

    Added the file upload functionality

--- book-network/src/main/java/org/tan/booknetwork/book/file/FileUtils.java
+++ book-network/src/main/java/org/tan/booknetwork/book/file/FileUtils.java
@@ -1,2 +1,23 @@
-package org.tan.booknetwork.book.file;public class FileUtils {
+package org.tan.booknetwork.book.file;
+
+import io.jsonwebtoken.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.nio.file.Path;
+
+@Slf4j
+public class FileUtils {
+
+    public static byte[] readFileFromLocation(String fileUrl) {
+        if(StringUtils.isBlank(fileUrl))
+            return null;
+        try{
+            Path filePath = new File(fileUrl).toPath();
+        }catch(IOException e){
+            log.warn("No file found in the path {} ",fileUrl);
+        }
+        return null;
+    }
 }
